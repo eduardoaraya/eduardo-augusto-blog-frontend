4249823e2f0126dec3579b47bd3c4ddd7f71fe16
eduardoaraya <eduardojezine@gmail.com>
2023-02-05 18:50:33 -0300

    [feature]: Including a env to url api

--- app-front/src/environments/environment.prod.ts
+++ app-front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-declare const process: { env?: { [key: string]: string } };
+declare const process: { env: { [key: string]: string } };
 
 export const environment = {
   production: true,
-  api: 'https://eduardo-augusto-project-hurll5kjwq-uc.a.run.app/',
+  api: process.env["URL_API"] ?? 'http://localhost:8080/'
 };
